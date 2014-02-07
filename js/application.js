9a49ce05f8be8043410fa1779ad6e2e23cec517b
Ryan Stenhouse <ryan@ryanstenhouse.eu>
2014-02-07 18:57:50 +0000

    Quickly commit things before I head home

---
+++ js/application.js
@@ -0,0 +1,41 @@
+
+// Multipliers based on how hungry you think people are
+//
+var big_eaters_multiplier     = 2;
+var medium_eaters_multiplier  = 1.5;
+var small_eaters_multiplier   = 0.7;
+
+// The base number of slices a normal person will be eating
+//
+var slices_per_person         = 2.5;
+
+// Multiplier for this being a work lunch (on the company)
+//
+var work_lunch_multiplier     = 1.5;
+
+// Multiplier for there being sides present
+//
+var sides_present_multiplier = 0.6;
+
+
+var pizza_chain_data  = {
+  "Dominos" : {
+    "large"   : 10,
+    "medium"  : 8,
+    "small"   : 6
+  },
+
+  "Pizza Hut" : {
+    "large"   : 10,
+    "medium"  : 8,
+    "small"   : 6
+
+  },
+
+  "Pappa John's" : {
+    "large"   : 10,
+    "medium"  : 8,
+    "small"   : 6
+  }
+};
+
