fe3df6a0a3fbef5028759d8dd70092aff82c8267
Ryan Stenhouse <hello@nekotachi.co.uk>
2024-06-29 16:51:07 +0900

    Skeleton Vite + Vue + Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: media, // or 'media' or 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
